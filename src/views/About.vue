a0f834758b165de92208afe9a52c4f4b1bc0e318
Ridwan Kustanto <ridwankustanto@gmail.com>
2018-08-07 11:15:40 +0700

    RK: move todos array inside store

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <p>This is an about page</p>
+    <p>This repo made for implementing any kind of simple app using Quasar framework.</p>
   </q-page>
 </template>
 
