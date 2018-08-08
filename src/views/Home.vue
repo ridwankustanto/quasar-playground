d1f5c7fea27e5d72a8d1e60e627b3ac0fb0da5e6
Ridwan Kustanto <ridwankustanto@gmail.com>
2018-08-08 19:15:19 +0700

    RK: try include filter all, done and undone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,65 @@
 <template>
   <q-page>
-    <div class="row">
-      <div class="col-xs-12 col-8-md offset-2-md q-pa-xl">
-          <q-field
-          icon="add_circle_outline"
-          >
-            <q-input 
-            @keyup.enter="submit()" 
-            float-label="Add your list" 
-            :value="todo" 
-            @change="val => todo = val"
-             />
-          </q-field>
-          
-          <q-list 
-            class="q-mt-sm" 
-            highlight
-            no-border	
-          >
-            <q-list-header>Recent todo lists</q-list-header>
-            <template v-for="(value, index) in todoStorage">
-              <q-item v-if="value.done" :key="index">
-                <q-item-side left>
-                  <q-btn flat color="secondary" round icon="check_circle" @click="done(value.text, index)"></q-btn>
-                </q-item-side>
-                <q-item-main disabled><strike>{{value.text}}</strike></q-item-main>
-                <q-item-side right>
-                  <q-btn flat round icon="highlight_off" @click="deleteTodo(index)"></q-btn>
-                </q-item-side>
-              </q-item>
+    <div class="row justify-center">
 
-              <q-item v-else :key="index">
-                <q-item-side left>
-                  <q-btn flat round icon="check_circle" @click="done(value.text, index)"></q-btn>
-                </q-item-side>
-                <q-item-main :label="value.text" />
-                <q-item-side right>
-                  <q-btn flat round icon="highlight_off" @click="deleteTodo(index)"></q-btn>
-                </q-item-side>
-              </q-item>
-            </template>
-          </q-list>
-      </div>
+    <!-- FIlter -->
+    <div class="col-md-4 offset-md-8 q-mt-xl">
+      <q-btn-toggle
+        v-model="filter"
+        toggle-color="primary"
+        :options="[
+          {label: 'All', value: 'all'},
+          {label: 'Undone', value: 'undone'},
+          {label: 'Done', value: 'done'}
+        ]"
+      />
     </div>
-    
+
+    <!-- input -->
+    <div class="col-md-8 q-pa-xl">
+      <q-field
+      icon="add_circle_outline"
+      >
+        <q-input 
+        @keyup.enter="submit()" 
+        float-label="Add your list" 
+        :value="todo" 
+        @change="val => todo = val"
+          />
+      </q-field>
+
+
+    <!-- list todo -->
+      <q-list 
+        highlight
+        no-border	
+      >
+        <q-list-header>Recent todo lists</q-list-header>
+        <template v-for="(value, index) in todoStorage">
+          <q-item v-if="value.done" :key="index">
+            <q-item-side left>
+              <q-btn flat color="secondary" round icon="check_circle" @click="done(value.text, index)"></q-btn>
+            </q-item-side>
+            <q-item-main disabled><strike>{{value.text}}</strike></q-item-main>
+            <q-item-side right>
+              <q-btn flat round icon="highlight_off" @click="deleteTodo(index)"></q-btn>
+            </q-item-side>
+          </q-item>
+
+          <q-item v-else :key="index">
+            <q-item-side left>
+              <q-btn flat round icon="check_circle" @click="done(value.text, index)"></q-btn>
+            </q-item-side>
+            <q-item-main :label="value.text" />
+            <q-item-side right>
+              <q-btn flat round icon="highlight_off" @click="deleteTodo(index)"></q-btn>
+            </q-item-side>
+          </q-item>
+        </template>
+      </q-list>
+    </div>      
+
+    </div>
   </q-page>
 </template>
 
@@ -55,12 +71,13 @@ export default {
   name: "PageHome",
   data() {
     return {
-      todo: ""
+      todo: "",
+      filter: null
     };
   },
   methods: {
     submit() {
-      this.$store.state.todos.push({ text: this.todo, done: false });
+      this.$store.state.todos.unshift({ text: this.todo, done: false });
       console.log(this.$store.state.todos);
 
       return (this.todo = "");
@@ -69,12 +86,26 @@ export default {
       return this.$store.state.todos.splice(index, 1);
     },
     done: function(val, index) {
-      return (this.$store.state.todos[index].done = !this.$store.state.todos[index].done);
+      return (this.$store.state.todos[index].done = !this.$store.state.todos[
+        index
+      ].done);
     }
   },
   computed: {
     todoStorage() {
-      return this.$store.getters.todoStorage;
+      switch (this.filter) {
+        case "all":
+          return this.$store.getters.todoStorage;
+          break;
+
+        case "done":
+          return this.$store.getters.todoStorage.filter(todo => todo.done == true);
+          break;
+
+        default:
+          return this.$store.getters.todoStorage.filter(todo => todo.done == false);
+          break;
+      }
     }
   }
 };
